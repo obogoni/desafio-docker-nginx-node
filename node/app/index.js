b047460309f6653f00e7b3d0d69293ddf7ba83fd
root <root@DESKTOP-HP159AT.triadesolucoes.com.br>
2024-07-08 23:06:15 -0300

    Primeiro commit

---
+++ node/app/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const conn = mysql.createConnection(config)
+
+const sql = `INSERT INTO Users(Name) VALUES ('Otavio')`
+
+conn.query(sql)
+
+const getListOfNames = async () => { 
+    const query = `SELECT * FROM people`;
+    const [result] = await connection.execute(query);
+    console.log(result)
+    return result;
+};
+
+app.get('/', (req, res) =>{
+
+	conn.query('SELECT name FROM Users', (err, results) => {
+    
+		if (err) {
+      
+			console.error('Erro:', err);
+			res.status(500).send('Erro!');
+			return;
+    		}
+
+    		let htmlList = '<ul>';
+    		
+		results.forEach(row => {
+      			htmlList += `<li>${row.name}</li>`;
+    		});
+    
+		htmlList += '</ul>';
+
+	
+    	res.send('<h1>Full Cycle!</h1>' + htmlList);
+  });
+
+});
+
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port)
+});
